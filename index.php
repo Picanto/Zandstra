762b1e0a10bec8ac4a356d69b6594fc9dcb9d093
picanto <compot2004@yandex.ru>
2019-04-06 23:31:55 +0300

    change title

--- index.php
+++ index.php
@@ -7,3 +7,12 @@ class ShopProduct
 	public $producerFirstName		= "Имя автора";
 	public $price					= 0;
 }
+
+$product1 = new ShopProduct();
+$product2 = new ShopProduct();
+
+$product1->title = "Собачье сердце";
+$product2->title = "Ревизор";
+
+var_dump($product1);
+var_dump($product2);
