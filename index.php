703a7af58ebaeb898b5aa07b26c62f0b31c347af
picanto <compot2004@yandex.ru>
2019-04-07 00:24:17 +0300

    add method getProducer

--- index.php
+++ index.php
@@ -6,13 +6,19 @@ class ShopProduct
 	public $producerMainName		= "Фамилия автора";
 	public $producerFirstName		= "Имя автора";
 	public $price					= 0;
+
+	function getProducer()
+	{
+		return 	"{$this->producerFirstName} "
+				."{$this->producerMainName}";	
+	}
+
 }
 
 $product1 = new ShopProduct();
+print $product1->title;
+
 $product2 = new ShopProduct();
 
 $product1->title = "Собачье сердце";
 $product2->title = "Ревизор";
-
-var_dump($product1);
-var_dump($product2);
