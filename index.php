37ab2e1e3fc95e1329bfb272b6d424a0f5c12b3c
picanto <compot2004@yandex.ru>
2019-04-06 23:09:21 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+class ShopProduct
+{
+	public $title					= "Стандартный товар";
+	public $producerMainName		= "Фамилия автора";
+	public $producerFirstName		= "Имя автора";
+	public $price					= 0;
+}
